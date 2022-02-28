0e41ce421811d70497833a84c1690b00e3af8247
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-01 00:34:29 +0530

    Update ExampleUnitTest.java

--- React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/test/java/com/medi360/ExampleUnitTest.java
+++ React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/test/java/com/medi360/ExampleUnitTest.java
@@ -3,7 +3,7 @@ package com.medi360;
 import org.junit.Test; 
 
 import static org.junit.Assert.*; 
-
+ 
 /**    
  * To work on unit tests, switch the Test Artifact in the Build Variants view.  
  */  
