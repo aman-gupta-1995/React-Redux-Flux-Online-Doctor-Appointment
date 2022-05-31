e14d8298a0c05f8c69cdbace2a05caa1c8c4560c
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 13:54:13 +0530

    Update PreviousAppintmentListAdapter.java

--- React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/main/java/com/medi360/Adapter/PreviousAppintmentListAdapter.java
+++ React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/main/java/com/medi360/Adapter/PreviousAppintmentListAdapter.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;   
 import android.view.ViewGroup; 
 import android.widget.BaseAdapter;  
-import android.widget.TextView;
+import android.widget.TextView; 
 
 import com.medi360.Model.AppointmentData;
 import com.medi360.R;
