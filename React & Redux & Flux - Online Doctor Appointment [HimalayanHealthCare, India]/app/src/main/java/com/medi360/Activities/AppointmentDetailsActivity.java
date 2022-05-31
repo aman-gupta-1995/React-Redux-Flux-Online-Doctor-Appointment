93bbb23ce827ea2563b3582bdd70bc6b362f0d50
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-31 13:55:20 +0530

    Update AppointmentDetailsActivity.java

--- React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/main/java/com/medi360/Activities/AppointmentDetailsActivity.java
+++ React & Redux & Flux - Online Doctor Appointment [HimalayanHealthCare, India]/app/src/main/java/com/medi360/Activities/AppointmentDetailsActivity.java
@@ -3,7 +3,7 @@ package com.medi360.Activities;
 import android.app.Activity;
 import android.app.AlertDialog; 
 import android.app.Dialog; 
-import android.app.ProgressDialog; 
+import android.app.ProgressDialog;  
 import android.content.Context;  
 import android.content.DialogInterface; 
 import android.content.Intent;
